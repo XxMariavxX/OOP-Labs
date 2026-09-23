aec10d016d7a38f45ce97f3dbac3efa7933ab248
Maria <synevicmaria13@gmail.com>
2026-09-23 22:53:58 +0300

    feat: remove preview for dot

--- Lab02/src/js/shapes/DotShape.js
+++ Lab02/src/js/shapes/DotShape.js
@@ -6,7 +6,7 @@ export class DotShape extends Shape {
   constructor(x1=0, y1=0) {
     super(x1, y1, x1, y1);
   }
-  draw(ctx, isPreview = false) {
+  draw(ctx) {
     const radius = 4;
 
     ctx.save();
@@ -14,16 +14,9 @@ export class DotShape extends Shape {
     ctx.strokeStyle = "black";
     ctx.fillStyle = "black";
     ctx.lineWidth = 1;
-    ctx.setLineDash(isPreview ? [5, 4] : []);
     ctx.arc(this.x1, this.y1, radius, 0, 2 * Math.PI);
     ctx.closePath();
-
-    if (isPreview) {
-      ctx.stroke();
-    } else {
-      ctx.fill();
-    }
-
+    ctx.fill();
     ctx.restore();
   }
 }
